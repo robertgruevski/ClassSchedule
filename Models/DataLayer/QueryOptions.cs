1a13202ec8900ee48d546e38ca0b36e3b0ecc771
robertgruevski <robertgruevski@outlook.com>
2025-01-29 15:33:17 -0500

    Query options to work with generics

---
+++ Models/DataLayer/QueryOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace ClassSchedule.Models.DataLayer
+{
+	public class QueryOptions<T>
+	{
+		public Expression<Func<T, Object>> OrderBy { get; set; }
+		public Expression<Func<T, Object>> ThenOrderBy { get; set; }
+		public Expression<Func<T, bool>> Where { get; set; }
+		private string[] includes = Array.Empty<string>();
+		public string Includes
+		{
+			set => includes = value.Replace(" ", "").Split(',');
+		}
+		public string[] GetIncludes() => includes;
+		public bool HasWhere => Where != null;
+		public bool HasOrderBy => OrderBy != null;
+		public bool HasThenOrderBy => ThenOrderBy != null;
+	}
+}
