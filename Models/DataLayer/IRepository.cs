19036a31d9eaf41be0bd0e43cb529b538d059791
robertgruevski <robertgruevski@outlook.com>
2025-01-29 15:57:35 -0500

    Implementing repository pattern

---
+++ Models/DataLayer/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassSchedule.Models.DataLayer
+{
+	public interface IRepository<T> where T : class
+	{
+		IEnumerable<T> List(QueryOptions<T> options);
+		T? Get(int id); // Get Type by ID
+		T? Get(QueryOptions<T> options); // Get Type with LINQ query
+		void Insert(T entity); // Create
+		void Update(T entity); // Update
+		void Delete(T entity); // Delete
+		void Save(T entity); // Save
+	}
+}
---
+++ Models/DataLayer/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClassSchedule.Models.DataLayer
+{
+	public class Repository<T> where T : class
+	{
+		protected ClassScheduleContext context { get; set; }
+		private DbSet<T> dbSet { get; set; }
+		public Repository(ClassScheduleContext ctx)
+		{
+			this.context = ctx;
+			dbSet = context.Set<T>();
+		}
+		public void Delete(T entity) => dbSet.Remove(entity);
+
+		public T? Get(int id) => dbSet.Find(id);
+
+		public T? Get(QueryOptions<T> options)
+		{
+			IQueryable<T> query = dbSet;
+			foreach(string include in options.GetIncludes())
+			{
+				query = query.Include(include);
+			}
+			if (options.HasWhere)
+				query = query.Where(options.Where);
+			return query.FirstOrDefault();
+		}
+
+		public void Insert(T entity) => dbSet.Add(entity);
+
+		public IEnumerable<T> List(QueryOptions<T> options)
+		{
+			IQueryable<T> query = dbSet;
+			foreach (string include in options.GetIncludes())
+			{
+				query = query.Include(include);
+			}
+			if (options.HasWhere)
+				query = query.Where(options.Where);
+			if (options.HasOrderBy)
+			{
+				if (options.HasThenOrderBy)
+				{
+					query = query.OrderBy(options.OrderBy).ThenBy(options.ThenOrderBy);
+				}
+				else
+				{
+					query = query.OrderBy(options.OrderBy);
+				}
+			}
+			return query.ToList();
+		}
+		public void Save() => context.SaveChanges();
+		public void Update(T entity) => dbSet.Update(entity);
+	}
+}
